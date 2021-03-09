383a5686afe5b89530cbc87e810e19161aad489e
raphaelchalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2021-03-09 16:56:19 +0100

    Ajout post test

--- _site/assets/javascripts/lunr/lunr.store.js
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "La 5G, c'est quoi?",
+        "excerpt":"Jelly-filled donuts are my favorite type of donuts because I like getting them without knowing what’s inside of them. I like to be surprised the moment I bite into them :) ","categories": [],
+        "tags": [],
+        "url": "https://raphaelchalicarne.github.io/environnement-5g/2021/02/26/la-5g-cest-quoi.html"
+      }]
